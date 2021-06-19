46474148f8c2fda715ca3097805063a12d9ec04e
Maro <omaralonso27@gmail.com>
2021-06-18 20:00:59 -0500

    feat:get team

--- flutter_application_teammatch/lib/pages/team_network_page.dart
+++ flutter_application_teammatch/lib/pages/team_network_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_teammatch/constants/teams_api.dart';
 import 'package:flutter_application_teammatch/models/team.dart';
 import 'package:flutter_application_teammatch/pages/team_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TeamNetworkPage extends StatefulWidget {
   @override
@@ -11,15 +12,15 @@ class TeamNetworkPage extends StatefulWidget {
 class _TeamNetworkPageState extends State<TeamNetworkPage> {
   @override
   Widget build(BuildContext context) => Container(
-    decoration: BoxDecoration(
+        decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/header_home.jpg'),
                 fit: BoxFit.cover)),
-    child: Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Search Your New Team'),
-      ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text('Search Your New Team'),
+          ),
           body: FutureBuilder<List<Team>>(
             future: TeamsApi.getTeams(),
             builder: (context, snapshot) {
@@ -38,7 +39,7 @@ class _TeamNetworkPageState extends State<TeamNetworkPage> {
             },
           ),
         ),
-  );
+      );
 
   Widget buildTeams(List<Team> teams) => ListView.builder(
         physics: BouncingScrollPhysics(),
@@ -46,16 +47,26 @@ class _TeamNetworkPageState extends State<TeamNetworkPage> {
         itemBuilder: (context, index) {
           final team = teams[index];
 
-          return ListTile(
-            onTap: () => Navigator.of(context).push(MaterialPageRoute(
-              builder: (BuildContext context) => TeamPage(team: team),
-            )),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(team.image_url),
-              backgroundColor: Colors.white,
+          return Card(
+            elevation: 10.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            child: Column(
+              children: [
+                ListTile(
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => TeamPage(team: team),
+                  )),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(team.image_url),
+                    backgroundColor: Colors.white,
+                  ),
+                  trailing: FaIcon(FontAwesomeIcons.arrowRight),
+                  title: Text(team.name),
+                  subtitle: Text(team.location),
+                ),
+              ],
             ),
-            title: Text(team.name),
-            subtitle: Text(team.location),
           );
         },
       );
