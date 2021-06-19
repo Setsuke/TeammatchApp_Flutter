7195a1111448b8bb3beee88a5250aa85473d24df
Maro <omaralonso27@gmail.com>
2021-06-19 17:15:48 -0500

    fix: pubspec

--- flutter_application_teammatch/lib/models/team.dart
+++ flutter_application_teammatch/lib/models/team.dart
@@ -1,17 +1,29 @@
 import 'package:meta/meta.dart';
 
 class Team {
+  final int id;
   final String name;
   final String location;
   final String image_url;
 
-  const Team({
+  Team({
+    @required this.id,
     @required this.name,
     @required this.location,
     @required this.image_url,
   });
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'location': location,
+      'image_url': image_url,
+    };
+  }
+
   static Team fromJson(json) => Team(
+        id: json['id'],
         name: json['name'],
         location: json['location'],
         image_url: json['image_url'],
