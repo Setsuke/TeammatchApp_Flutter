7c031f7b31ebe649309f32a2c5ab1524878de02f
Maro <omaralonso27@gmail.com>
2021-06-24 18:46:57 -0500

    feat: Login

--- flutter_application_teammatch/lib/constants/tournaments_api.dart
+++ flutter_application_teammatch/lib/constants/tournaments_api.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 
 class TournamentsApi {
   static Future<List<Tournament>> getTournaments() async {
-    final url =
-        'https://api.pandascore.co/lol/leagues?page[size]=5&token=jGhCbVNlvEPNoT5DUWhUD2faknOSdqJOJZ2_3oGtwkzFX09J684';
+    final url = Uri.parse(
+        'https://api.pandascore.co/lol/leagues?page[size]=5&token=jGhCbVNlvEPNoT5DUWhUD2faknOSdqJOJZ2_3oGtwkzFX09J684');
     final response = await http.get(url);
     final body = json.decode(response.body);
 
